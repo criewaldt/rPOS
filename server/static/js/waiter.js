d7ff58463a63e653eb726435c0d01582ece95345
reliableJARED <jarednugent@gmail.com>
2016-04-07 14:00:32 -0400

    waiter.js, waiter.html and testing server for socket

--- server/static/js/waiter.js
+++ server/static/js/waiter.js
@@ -36,21 +36,32 @@ function ButtonBuilder() {
 
 $(document).ready(function(){
 	mySocket = io.connect();//create new websocket, 
-
-	/* FUNCTION TO SEND MESSAGE ON CLICK*/
+	
+	/* ON LOAD build the buttons on the page
+	server will reply to 'buttonBuilder' with appropriate JSON*/
+	mySocket.send('buttonBuilder');
+	
+	/* EXAMPLE function to send data on click*/
 	$(".button").click(function(event){
-		mySocket.send('buttonBuilder');
+		mySocket.send({'TEST':'Data Here'});
 	});
 	
-	/* FUNCTION TO READ INBOUND DATA */
+	/* FUNCTION TO READ INBOUND DATA*/
 	mySocket.on('message', function(msg) {
 		  //console.log(msg['buttonBuilder']);	
 		  if (msg['buttonBuilder']) {
-		  	console.log(msg);
-		  	console.log(CountMyItems(msg));
-		  	console.log(CountMyItems(msg.buttonBuilder));
-		  	var MyTest = msg.buttonBuilder.burgers;
-		  	console.log(MyTest[0].BBQ);
+		  	var MyTest = msg.buttonBuilder;
+		  	//console.log(MyTest);
+		  	console.log(Object.keys(MyTest));
+		  	var myKeys = Object.keys(MyTest);
+		  	console.log(myKeys[0]);
+		  	for (i=0; i<myKeys.length; i++) {
+		  		console.log(MyTest[myKeys[i]]);
+		  	}
+		  //	console.log(msg);
+		  //	console.log(CountMyItems(msg));
+		  //	console.log(CountMyItems(msg.buttonBuilder));
+		  	//console.log(MyTest[0].BBQ);
 		  	for (index in MyTest) {
 		  		$('#testArea').append('<li><a href="#" item="'+MyTest[index].BBQ+'" price="'+MyTest[index].CHZ+'">TEST</a></li>');
 		  		//$('#testArea').append("<p>" + MyTest[index].BBQ+"</p>");
