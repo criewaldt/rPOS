5077b6a47bcde48c4dd5b228254ff87918bc9ca4
reliableJARED <jarednugent@gmail.com>
2016-04-07 22:27:43 -0400

    waiter.js work

--- server/flaskSocketIO_JMNuses4test.py
+++ server/flaskSocketIO_JMNuses4test.py
@@ -3,10 +3,41 @@ from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO
 from flask.ext.socketio import emit, send
 import time
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+#when live this JSON should be built by reading an excel sheet
+buttons = {'buttonBuilder': 
+                 {'entre':
+                      {'burger':
+                           {'price':'10.50'},
+                       'veggie burger':
+                           {'price':'9.50'},
+                       'mods':
+                           {'cheese':'1.50',
+                            'bacon':'0.75',
+                            'pickles':'0.50',
+                            },
+                       
+                  'pasta':
+                       {'parmagan':
+                            {'price':'10.50'},
+                        'alfrado':
+                            {'price':'9.50'},
+                        'mods':
+                           {'sauce':'2.00',
+                            'broccoli':'1.75',
+                            'garlic':'0.50',
+                            }
+                        }
+                       }
+                  }
+           }
+                  
+           
 
 @app.route('/')
 def index():
@@ -21,18 +52,20 @@ def test_connect():
 
 @socketio.on('message')
 def handle_message(message):
-    if message == 'buttonBuilder':
-        send({'buttonBuilder': \
-                 {'sandwiches':\
-                      [{'burger':'10.50',\
-                       'club':9.50}],\
-                  'pasta':\
-                       [{'parmagan':9.25,\
-                        'alfrado':8.75}]}})
+    dataIN = json.loads(message)
+    print dataIN
+    print dataIN.itervalues().next()
+    print dataIN.iterkeys().next()
+    if dataIN.iterkeys().next() == "buttonBuilder":
+        send(json.dumps(buttons))
         print "button setup sent"
-    if message['TEST']:
-        print message['TEST']
-    else: print message
+
+    else:
+        print message
+        print type(message)
+        print dataIN
+        print type(dataIN)
+    
    
 
 if __name__ == '__main__':
