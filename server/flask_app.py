9cd317cf7d78b576df8bdbf43e213b14210676bd
Christopher Riewaldt <chrisr@Christophers-MacBook-Pro.local>
2016-04-14 16:12:28 -0400

    changed relative path to db file

--- server/flask_app.py
+++ server/flask_app.py
@@ -15,7 +15,7 @@ DATABASE = 'db.db'
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect('db/db.sqlite')
+        db = g._database = sqlite3.connect('../db/db.sqlite')
         db.row_factory = make_dicts
     return db
 
